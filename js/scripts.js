f4f5de9aeeecd60dd025d89ea961b0091bdc5560
Igor Larbac0 <igorcabral59@hotmail.com>
2024-07-28 19:58:26 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,48 @@
+const menu = document.querySelector(".menu");
+const btnMenu = document.querySelector(".btn-menu");
+const container__links = document.querySelector(".container__links");
+
+function handleButtonClick(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  event.stopPropagation();
+  menu.classList.toggle("active");
+  handleClickOutside(menu, () => {
+    menu.classList.remove("active");
+    setAria();
+  });
+  setAria();
+}
+
+function handleClickOutside(targetElement, callback) {
+  const html = document.documentElement;
+  function handleHTMLClick(event) {
+    if (!targetElement.contains(event.target)) {
+      targetElement.removeAttribute("data-target");
+      html.removeEventListener("click", handleHTMLClick);
+      html.removeEventListener("touchstart", handleHTMLClick);
+      callback();
+    }
+  }
+  if (!targetElement.hasAttribute("data-target")) {
+    html.addEventListener("click", handleHTMLClick);
+    html.addEventListener("touchstart", handleHTMLClick);
+    targetElement.setAttribute("data-target", "");
+  }
+}
+
+function setAria() {
+  const isActive = menu.classList.contains("active");
+  btnMenu.setAttribute("aria-expanded", isActive);
+  if (isActive) {
+    btnMenu.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
+
 document.addEventListener('DOMContentLoaded', function () {
     let slideIndex = 0; // Inicializa o índice do slide para começar do primeiro slide
     const slides = document.querySelectorAll('.slide-item'); // Seleciona todos os elementos com a classe 'slide-item'
