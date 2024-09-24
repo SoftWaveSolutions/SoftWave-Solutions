17c28cd3c7eabb05f2038fbac732dd2090c44665
Igor Larbac0 <igorcabral59@hotmail.com>
2024-09-23 21:57:48 -0300

    Slide
    Adição do slide

--- js/scripts.js
+++ js/scripts.js
@@ -95,5 +95,36 @@ function showValues() {
             </Ul>
         </div>
     `;
-}
+};
 
+
+
+			const swiper = new Swiper(".swiper", {
+				spaceBetween: 1,
+				slidesPerView: 3,
+				navigation: {
+					nextEl: ".swiper-button-next",
+					prevEl: ".swiper-button-prev",
+				},
+				pagination: {
+					el: ".swiper-pagination",
+					type: "bullets",
+				},
+				breakpoints: {
+					// when window width is >= 320px
+					320: {
+						slidesPerView: 1,
+						spaceBetween: 20,
+					},
+					// when window width is >= 480px
+					480: {
+						slidesPerView: 2,
+						spaceBetween: 30,
+					},
+					// when window width is >= 640px
+					640: {
+						slidesPerView: 3,
+						spaceBetween: 40,
+					},
+				},
+			});
