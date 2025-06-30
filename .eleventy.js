1fdb428b63333eb99968be51853778fd935b651e
Igor Cabral <igorcabral59@hotmail.com>
2025-06-30 20:59:38 -0300

    .

--- .eleventy.js
+++ .eleventy.js
@@ -15,5 +15,5 @@ module.exports = function (eleventyConfig) {
 };
 
 eleventyConfig.addCollection("posts", function(collectionApi) {
-  return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
+  return collectionApi.getFilteredByGlob("posts/*.md").reverse();
 });
