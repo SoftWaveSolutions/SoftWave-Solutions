c2e83b93f08ba5d1b61e9074761c8580e3927c10
Igor Cabral <igorcabral59@hotmail.com>
2025-06-30 21:38:43 -0300

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,21 @@
 module.exports = function (eleventyConfig) {
+  // Copiar pastas públicas
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("_redirects");
 
+  // Coleção de posts (busca os .md da pasta posts/)
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("posts/*.md").reverse();
+  });
+
   return {
     dir: {
       input: ".",
-      includes: "_includes",
+      includes: "_includes", // ⚠️ você usou "includes", não "_includes"
       output: "_site"
     },
     markdownTemplateEngine: "njk"
   };
 };
-
-eleventyConfig.addCollection("posts", function(collectionApi) {
-  return collectionApi.getFilteredByGlob("posts/*.md").reverse();
-});
