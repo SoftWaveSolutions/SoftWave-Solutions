de37390345eb2a386f951d66f30481ea47866223
Igor Cabral <igorcabral59@hotmail.com>
2025-07-01 20:00:34 -0300

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,18 @@
 module.exports = function (eleventyConfig) {
-  // Copiar pastas públicas
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("_redirects");
 
-  // Coleção de posts (busca os .md da pasta posts/)
+  // Corrigindo a collection
   eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("posts/*.md").reverse();
+    return collectionApi.getFilteredByGlob("posts/**/index.md").reverse();
   });
 
   return {
     dir: {
       input: ".",
-      includes: "_includes", // ⚠️ você usou "includes", não "_includes"
+      includes: "_includes",
       output: "_site"
     },
     markdownTemplateEngine: "njk"
--- posts/teste/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Teste
-date: 2025-07-01T22:44:08.789Z
-thumbnail: discord.png
----
-T﻿este
